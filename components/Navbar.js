6335a2e8719599443c2379c735297fc761365f74
Adrian Mercer <50633416+brown51s@users.noreply.github.com>
2025-02-16 19:28:01 +0800

    feat(navbar): add contact link for improved navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,12 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="p-6 bg-gray-800 flex justify-between">
+    <nav className="p-6 bg-gray-800 flex justify-between items-center">
       <h1 className="text-2xl font-bold">SentinelDAO</h1>
       <div className="space-x-4">
-        <Link href="/about">About</Link>
-        <Link href="/features">Features</Link>
+        <Link href="/about" className="hover:underline">About</Link>
+        <Link href="/features" className="hover:underline">Features</Link>
+        <Link href="/contact" className="hover:underline">Contact</Link> {/* Added Contact link */}
       </div>
     </nav>
   );
