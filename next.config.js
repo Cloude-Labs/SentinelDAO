3dd4ab67673cd9e3f84ab70b2c87f474c8e0d54f
Adrian Mercer <50633416+brown51s@users.noreply.github.com>
2025-02-16 19:23:49 +0800

    chore(config): disable powered-by header and add external image support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     poweredByHeader: false, // Disabled Next.js powered-by header for security
+    images: {
+        domains: ['example.com'], // Added support for external image domains
+    },
 };
